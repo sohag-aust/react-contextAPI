ab6ee9cffc062bcd61ed92bcaac893c6e88ea33a
sohag-aust <frustratedcoder96@gmail.com>
2022-02-07 20:17:17 +0600

    Tut#14, Added localStorage for Movie Data

--- src/Contexts/MovieContext.js
+++ src/Contexts/MovieContext.js
@@ -1,11 +1,20 @@
-import React, {createContext, useState, useReducer} from 'react';
+import React, {createContext, useState, useReducer, useEffect} from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { MovieReducer } from '../Components/Reducers/MovieReducer';
 
 export const MovieContext = createContext();
 
 const MovieContextProvider = (props) => {
-    const [movies, dispatch] = useReducer(MovieReducer, []);
+    const getMoviesFromLocalStorage = () => {
+        const localData = localStorage.getItem('movies');
+        return localData ? JSON.parse(localData) : [];
+    };
+
+    const [movies, dispatch] = useReducer(MovieReducer, [], getMoviesFromLocalStorage); /**  3rd parameter is optional, i here used it for getting localStorage data and set it in initial state */
+
+    useEffect(() => {
+        localStorage.setItem('movies', JSON.stringify(movies));
+    }, [movies]);
 
     return (
         <MovieContext.Provider value={{
